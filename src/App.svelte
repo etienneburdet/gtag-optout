2dc04bc6a0090d6b36fe5f0c4edcf7282ba75c78
Etienne Burdet <etienne.burdet@gmail.com>
2021-03-17 12:46:56 +0100

    test optout gtags

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let name;
+	let ok = false;
+</script>
+
+<svelte:head>
+	{#if ok}
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'GA_MEASUREMENT_ID');
+		console.log('added gtags')
+		</script>
+	{/if}
+</svelte:head>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<span>Accept Gtags</span>
+<input type="checkbox" bind:checked={ok}>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
